ad9b5928e53ac4a35652be113f33f9cd5d8525a1
ManuelGallegoJ <92808245+ManuelGallegoJ@users.noreply.github.com>
2022-03-22 21:16:00 -0500

    Segundo intento

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ class Asiento:
         self.precio = precio
         self.registro = registro
 
-        def cambiarColor(color):
-            if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
-                self.color = color
+    def cambiarColor(self, color):
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
+            self.color = color
 
 class Motor:
     def __init__(self, numero, tipo, registro):
@@ -14,15 +14,15 @@ class Motor:
         self.tipo = tipo
         self.registro = registro
 
-        def cambiarRegistro(numero):
-            self.numero = numero
-        def asignarTipo(tipo):
-            if tipo == "electrico" or tipo == "gasolina":
-                self.tipo = tipo
+    def cambiarRegistro(self, numero):
+        self.numero = numero
+    def asignarTipo(self, tipo):
+        if tipo == "electrico" or tipo == "gasolina":
+            self.tipo = tipo
 
 class Auto:
     cantidadCreados = 0
-    def __init__(self, modelo, precio, asiento, marca, motor, registro, cantidadCreados):
+    def __init__(self, modelo, precio, asiento, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
         self.asiento = asiento
@@ -30,19 +30,19 @@ class Auto:
         self.motor = motor
         self.registro = registro
 
-        def cantidadAsientos(self):
-            c = 0
+    def cantidadAsientos(self):
+        c = 0
+        for i in self.asientos:
+            if type(i) == Asiento:
+                c += 1
+        return c
+
+    def verificarIntegridad(self):
+        if self.motor.registro == self.registro:
             for i in self.asientos:
                 if type(i) == Asiento:
-                    c += 1
-            return c
-            
-        def verificarIntegridad(self):
-            if self.motor.registro == self.registro:
-                for i in self.asientos:
-                    if type(i) == Asiento:
-                        if self.registro != self.asiento.registro:
-                            return "Las piezas no son originales"
-                return "Auto original"
-            else:
-                return "Las piezas no son originales"
+                    if self.registro != self.asiento.registro:
+                        return "Las piezas no son originales"
+            return "Auto original"
+        else:
+            return "Las piezas no son originales"
